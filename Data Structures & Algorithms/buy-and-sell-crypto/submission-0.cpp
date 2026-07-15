560123c4dbbf3f7d35b8c29ece11eef491141d5d
pramukhnayak <74535521+pramukhnayak7@users.noreply.github.com>
2026-07-15 23:47:52 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxP =0,minBuy=prices[0];
+        for(int& sell :prices){
+            maxP = max(maxP,sell-minBuy);
+            minBuy=min(minBuy,sell);
+        }
+        return maxP;
+    }
+};
